016c98ea5098fc8ecf2235d53d7b398619bd5921
ashwani <ashwani.saxena@adequateinfosoft.net>
2025-08-14 19:51:56 +0530

    update

--- src/main/java/com/mvp/ai11/HelloWorld.java
+++ src/main/java/com/mvp/ai11/HelloWorld.java
@@ -1,12 +1,12 @@
 package com.mvp.ai11;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.lang3.StringUtils;
 
 public class HelloWorld extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-    	Example obj = new Example();
         from("timer:java?period=1s")
-            .setBody(constant("Hello Camel K from Java!" + obj.getName()))
+            .setBody(constant("Hello Camel K from Java!" + StringUtils.lowerCase("Welcome")))
             .log("${body}");
     }
 }
